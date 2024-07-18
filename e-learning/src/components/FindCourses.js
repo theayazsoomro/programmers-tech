eb887282a71339bf7436101f4feff5c65c668749
Ayaz Soomro <ayazkhansoomro07@gmail.com>
2024-07-19 00:58:18 +0500

    Login and Registration forms linked with backend

--- e-learning/src/components/FindCourses.js
+++ e-learning/src/components/FindCourses.js
@@ -1,46 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBook, faClock, faStar } from "@fortawesome/free-solid-svg-icons";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-const courses = [
-  {
-    category: "Technology",
-    title: "Expand your career opportunities with Python",
-    price: "$55",
-    lessons: "10 Lessons",
-    duration: "5h 20m",
-    rating: "4.5",
-    instructor: "Linda Bacote",
-    imgSrc:
-      "https://www.learnfly.com/img/post_img/1335475250_1_5ev1xmjs2-sj4ddejfdnqa.png",
-  },
-  {
-    category: "Business",
-    title: "Effective business pages on social media",
-    price: "$35",
-    lessons: "13 Lessons",
-    duration: "3h 15m",
-    rating: "4.2",
-    instructor: "Jane Cooper",
-    imgSrc:
-      "https://www.learnfly.com/img/post_img/1335475250_1_5ev1xmjs2-sj4ddejfdnqa.png",
-  },
-  {
-    category: "Life Style",
-    title: "How to increase your social media engagement",
-    price: "$22",
-    lessons: "8 Lessons",
-    duration: "2h 17m",
-    rating: "4.2",
-    instructor: "Martin Scorsese",
-    imgSrc:
-      "https://www.learnfly.com/img/post_img/1335475250_1_5ev1xmjs2-sj4ddejfdnqa.png",
-  },
-];
+import axios from "axios";
 
 const FindCourses = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/api/courses");
+        setCourses(response.data);
+      } catch (error) {
+        console.error("Error fetching the courses", error);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <Container className="findCourses my-5">
       <h2 className="text-center my-5">
